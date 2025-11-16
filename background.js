42b0669f9c88cab6cb42082e734e4b2166e98299
Sumant Sogikar <sumant1122@gmail.com>
2025-11-16 12:34:18 +0530

    add backgroud changes

--- background.js
+++ background.js
@@ -1,30 +1,8 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log('Background script received message:', request);
-  if (request.action === 'saveSummary') {
-    const summary = request.summary;
-    const filename = request.filename;
+  console.log('Background script received a message.');
+  console.log('Request:', request);
+  console.log('Sender:', sender);
 
-    const blob = new Blob([summary], { type: 'text/markdown' });
-    const url = URL.createObjectURL(blob);
-
-    console.log('Calling chrome.downloads.download with filename:', filename);
-    chrome.downloads.download(
-      {
-        url: url,
-        filename: filename,
-        saveAs: true,
-      },
-      (downloadId) => {
-        if (chrome.runtime.lastError) {
-          console.error('Download failed:', chrome.runtime.lastError.message);
-          sendResponse({ success: false });
-        } else {
-          console.log('Download initiated with ID:', downloadId);
-          sendResponse({ success: true });
-        }
-      }
-    );
-    // Return true to indicate that the response will be sent asynchronously.
-    return true;
-  }
+  // Send a simple, immediate response to prevent port-closed error.
+  sendResponse({ success: true, message: 'Message received' });
 });
